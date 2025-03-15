3efc0e6b756680b427b352543d9153a91d3d5dba
Jamie Openshaw <github@openshaw.tech>
2025-03-15 20:37:07 +0000

    Added loading of .env file for AI API key(s) on start of nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- load the .env file for providing api key for code-companion plugin
+vim.cmd("Dotenv ~/.config/nvim/lua/plugins/.env")
